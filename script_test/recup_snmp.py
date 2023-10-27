82d307910163cebeaa94728eda05434c7f18bae0
Nicoprogrameur <nicolasravit@hotmail.fr>
2023-10-27 13:59:04 +0200

    add script for SNMP v3

--- script_test/recup_snmp.py
+++ script_test/recup_snmp.py
@@ -1,5 +1,6 @@
 import subprocess
 
+### Parti SNMP v2c
 comm = 'passprojet '
 addr = '192.168.176.2 '
 
@@ -14,6 +15,28 @@ sortie = process.stdout.read().decode()
 # Diviser la chaîne en une liste en fonction des sauts de ligne
 liste_lignes = sortie.split('\n')
 
+### Partie SNMP v3
+#./simple_SNMP_v3.sh 192.168.140.140 user1 SHA miracle2022 AES power2022 1.3.6.1.2.1.4.20.1.1
+addrv3 = '192.168.140.140 '
+userv3 = 'user1 '
+authProtocol = 'SHA '
+authKey = 'miracle2022 '
+privProtocol = 'AES '
+privKey = 'power2022 '
+
+# Exécution du script bash
+commande_v3 = './multi_SNMP_v3.sh ' + addrv3 + userv3 + authProtocol + authKey + privProtocol + privKey
+process_v3 = subprocess.Popen(commande_v3, shell=True, stdout=subprocess.PIPE)
+sortie_v3 = process_v3.stdout.read().decode()
+
+# Diviser la chaîne en une liste en fonction des sauts de ligne
+liste_lignes_v3 = sortie_v3.split('\n')
+
 # Afficher chaque ligne séparément
 for ligne in liste_lignes:
+    print(ligne)
+
+print('-----------------------------------')
+
+for ligne in liste_lignes_v3:
     print(ligne)
--- script_test/simple_SNMP_v3.sh
+++ script_test/simple_SNMP_v3.sh
@@ -10,7 +10,7 @@
 # $7 OID
 snmpwalk -v3 -l authPriv -u $2 -a $3 -A $4 -x $5 -X $6 $1 $7 \
 | while read line; do
-	msg_OID=$(echo $line | awk -F: '{print $2}' | awk '{$1=$1};1')
+	msg_OID=$(echo $line | awk -F': ' '{print $2}' | awk '{$1=$1};1')
 	printf "%s;" "$msg_OID"
 done
 printf "\n" 
