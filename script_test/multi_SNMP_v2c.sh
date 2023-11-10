f650b44336682e4b4f6ac6f65d7d1a037df4425c
Nicoprogrammeur <nicolasravit@hotmail.fr>
2023-11-10 23:09:08 +0100

    add docker snmp and OID sysContact

--- script_test/multi_SNMP_v2c.sh
+++ script_test/multi_SNMP_v2c.sh
@@ -8,12 +8,14 @@ addrIP=$2
 oid_list[0]="1.3.6.1.2.1.4.20.1.1"
 #oid_name
 oid_list[1]="1.3.6.1.2.1.1.5"
+#oid_contact
+oid_list[2]="1.3.6.1.2.1.1.4"
 #oid_descr
-oid_list[2]="1.3.6.1.2.1.1.1"
+oid_list[3]="1.3.6.1.2.1.1.1"
 #oid_location
-oid_list[3]="1.3.6.1.2.1.1.6"
+oid_list[4]="1.3.6.1.2.1.1.6"
 #oid_uptime
-oid_list[4]="1.3.6.1.2.1.1.3"
+oid_list[5]="1.3.6.1.2.1.1.3"
 
 # Boucle sur les OIDs
 for oid in "${oid_list[@]}"
